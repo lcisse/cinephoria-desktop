8dce5fca7ce68112820e0382ec9aa1a5c5ffafa6
lcisse <sotiba67@gmail.com>
2025-03-09 16:24:08 +0100

    packahe add to release

--- AppCinephoria-win32-x64/resources/app/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  send: (channel, data) => ipcRenderer.send(channel, data),
-  on: (channel, callback) => ipcRenderer.on(channel, (event, ...args) => callback(...args)),
-  once: (channel, callback) => ipcRenderer.once(channel, (event, ...args) => callback(...args)),
-});
-
